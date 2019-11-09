ae4d6a924f931a62b2f79e9078da1856098fe579
Johannes Wezel <johanneswezel@gmail.com>
2019-11-09 16:01:59 +0100

    initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+    "eta adapter settings": {
+        "en": "Adapter settings for eta",
+        "de": "Adaptereinstellungen für eta",
+        "ru": "Настройки адаптера для eta",
+        "pt": "Configurações do adaptador para eta",
+        "nl": "Adapterinstellingen voor eta",
+        "fr": "Paramètres d'adaptateur pour eta",
+        "it": "Impostazioni dell'adattatore per eta",
+        "es": "Ajustes del adaptador para eta",
+        "pl": "Ustawienia adaptera dla eta",
+        "zh-cn": "eta的适配器设置"
+    },
+    "option1": {
+        "en": "option1",
+        "de": "Option 1",
+        "ru": "Опция 1",
+        "pt": "Opção 1",
+        "nl": "Optie 1",
+        "fr": "Option 1",
+        "it": "opzione 1",
+        "es": "Opción 1",
+        "pl": "opcja 1",
+        "zh-cn": "选项1"
+    },
+    "option2": {
+        "en": "option2",
+        "de": "Option 2",
+        "ru": "option2",
+        "pt": "opção 2",
+        "nl": "Optie 2",
+        "fr": "Option 2",
+        "it": "opzione 2",
+        "es": "opcion 2",
+        "pl": "Opcja 2",
+        "zh-cn": "选项2"
+    }
+};
