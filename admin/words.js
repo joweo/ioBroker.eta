0d429d6cfac3c5986c29565aefa1e7744bbf8108
Johannes Wezel <johanneswezel@gmail.com>
2019-11-11 10:00:52 +0100

    commit

--- admin/words.js
+++ admin/words.js
@@ -14,28 +14,12 @@ systemDictionary = {
         "pl": "Ustawienia adaptera dla eta",
         "zh-cn": "eta的适配器设置"
     },
-    "option1": {
-        "en": "option1",
-        "de": "Option 1",
-        "ru": "Опция 1",
-        "pt": "Opção 1",
-        "nl": "Optie 1",
-        "fr": "Option 1",
-        "it": "opzione 1",
-        "es": "Opción 1",
-        "pl": "opcja 1",
-        "zh-cn": "选项1"
+    "host": {
+        "en": "ETA Touch IP/Hostname",
+        "de": "ETA Touch IP/Hostname 1"
     },
-    "option2": {
-        "en": "option2",
-        "de": "Option 2",
-        "ru": "option2",
-        "pt": "opção 2",
-        "nl": "Optie 2",
-        "fr": "Option 2",
-        "it": "opzione 2",
-        "es": "opcion 2",
-        "pl": "Opcja 2",
-        "zh-cn": "选项2"
+    "post": {
+        "en": "Port",
+        "de": "Port"
     }
 };
